7b4e80029ad59ea7fa9be4eea1f0a8fb7f542907
Frank Escobedo <fescobedo@mediatemple.net>
2015-12-26 17:25:34 -0800

    8 part b solution

--- Advent_Day_8_Matchsticks/characters_of_code.py
+++ Advent_Day_8_Matchsticks/characters_of_code.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python3
+import re
 
 
 def main():
     inputs = get_input_file()
     total = 0
+    partb_total = 0
     for line in inputs:
-        total += difference(line.rstrip("\n"))
+        line = line.rstrip("\n")
+        total += difference(line)
+        partb_total += encode_all_the_things(line)
 
-    print(total)
+    print("part a:{0}\npart b:{1}".format(total, partb_total))
 
 
 def difference(line):
     # print("{0}:{1}\t{2}:{3}".format(
     #    line, len(line), eval(line), len(eval(line))))
     return len(line) - len(eval(line))
+
+
+def encode_all_the_things(line):
+    # line = line.startswith(
+    # encoded_line = "\"" + re.escape(line) + "\""
+    # print("{0} -- {1}".format(line,encoded_line ))
+    return len("\"" + re.escape(line) + "\"") - len(line)
 
 
 def get_input_file():
